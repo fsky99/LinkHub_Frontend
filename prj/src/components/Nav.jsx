f810bc4636f7a1a6aed16653553cafe5610dafdc
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-02-29 16:43:08 +0300

    added Nav bar

---
+++ prj/src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+import "../App.css"
+
+const Nav = ({ user }) => {
+  console.log("user:",user)
+
+  const userOptions = (
+    <nav>
+      {/* <img src="https://picsum.photos/seed/picsum/200/300" alt="" /> */}
+      <Link to="/">LinkHub</Link>
+      <Link to="/">Home</Link>
+      <Link to="/following">Following posts</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/signout">Sign out</Link>
+      {/* Create Post */}
+    </nav>
+  )
+
+  const publicOptions = (
+    <nav>
+      {/* <img src="https://picsum.photos/seed/picsum/200/300" alt="" /> */}
+      <Link to="/">LinkHub</Link>
+      <Link to="/">Home</Link>
+      <Link to="/signup">Sign up</Link>
+    </nav>
+  )
+
+  return <header>{user ? userOptions : publicOptions}</header>
+}
+
+export default Nav
---
+++ prj/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import '../App.css'
+
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <nav id="sidebar">
+        <div class="sidebar-header">
+         <a href="#">Edit Profile</a> 
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Profile
