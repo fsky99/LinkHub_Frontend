d7626ef1f4926f3f23438aa4652138c79c36a344
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-03-01 10:31:10 +0300

    modify register component

--- prj/src/components/Register.jsx
+++ prj/src/components/Register.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  let navigate = useNavigate()
+
   const [formValues, setFormValues] = useState({
     name: '',
     email: '',
@@ -14,6 +18,19 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    await RegisterUser({
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password
+    })
+    setFormValues({
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    })
+
+    navigate('/signin')
   }
 
   return (
