d23ee2b239ae94c26ee6ef5a38a3138fa53a106a
fatima <fatimafouad9999@gmail.com>
2024-03-03 21:51:38 +0300

    nav bar edited

--- prj/src/components/Nav.jsx
+++ prj/src/components/Nav.jsx
@@ -1,17 +1,29 @@
-import { Link } from 'react-router-dom'
-import '../App.css'
+import { Link } from "react-router-dom"
+import "../App.css"
 
 const Nav = ({ user, handleLogOut }) => {
-  console.log('user:', user)
+  console.log("user:", user)
 
   const userOptions = (
     <nav>
       {/* <img src="https://picsum.photos/seed/picsum/200/300" alt="" /> */}
-      <Link to="/">LinkHub</Link>
-      <Link to="/">Home</Link>
-      <Link to="/followingPosts">Following posts</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/signin" onClick={handleLogOut}>
+      <Link className="linkstochange" to="/">
+        LinkHub
+      </Link>
+
+      <Link className="linkstochange" to="/">
+        Home
+      </Link>
+
+      <Link className="linkstochange" to="/followingPosts">
+        Following posts
+      </Link>
+
+      <Link className="linkstochange" to="/profile">
+        Profile
+      </Link>
+
+      <Link className="linkstochange" to="/signin" onClick={handleLogOut}>
         Sign out
       </Link>
       {/* Create Post */}
