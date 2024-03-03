6df1c3ab20b23f86aef65659c4100265570f0779
fatima <fatimafouad9999@gmail.com>
2024-03-03 20:09:14 +0300

    edited side bar

--- prj/src/components/Sidebar.jsx
+++ prj/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const Sidebar = () => {
   const BASE_URL = import.meta.env.VITE_BASE_URL
   const [listUsers, setListUsers] = useState([])
-  const [searchUsers, setSearchUsers] = useState('')
+  const [searchUsers, setSearchUsers] = useState("")
 
   useEffect(() => {
     getUsers()
@@ -16,24 +16,28 @@ const Sidebar = () => {
   }
 
   const handleChange = (e) => {
-    if (e.target.name === 'search') {
+    if (e.target.name === "search") {
       setSearchUsers(e.target.value)
     }
   }
 
   return (
     <div>
-      <aside  >
-        <div >
+      <aside>
+        <div>
           <div id="search-users">
-            <input
-              onChange={handleChange}
-              type="text"
-              name="search"
-              id="search"
-            />
+            <div class="ss">
+              <input
+                type="text"
+                name="search"
+                class="inputSearch"
+                id="search"
+                onChange={handleChange}
+                placeholder="Search"
+              />
+            </div>
           </div>
-          <menu >
+          <menu>
             <div>
               {listUsers.map(
                 (usr) =>
@@ -41,8 +45,8 @@ const Sidebar = () => {
                     .toLowerCase()
                     .includes(searchUsers.toLowerCase()) && (
                     <p key={usr._id}>
-                      <a href="#">{usr.userName}</a>
-                      <a href="">Visit</a>
+                      <a href="#" className="hideHyperlink">{usr.userName}</a> 
+                      {/* <a href="" className="hideHyperlink">Visit</a> */}
                     </p>
                   )
               )}
