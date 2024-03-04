7805d2999683d083c43eae5ea28ff0f1d41dbc95
fatima <fatimafouad9999@gmail.com>
2024-03-04 14:03:58 +0300

    post in nav bar'

--- prj/src/App.jsx
+++ prj/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from 'react'
-import { Route, Routes } from 'react-router'
+import { useState, useEffect } from "react"
+import { Route, Routes } from "react-router"
 
-import './App.css'
-import { checkSession } from './services/Auth'
-import Nav from './components/Nav'
-import Profile from './components/Profile'
-import SignIn from './components/SignIn'
-import Register from './components/Register'
-import Home from './components/Home'
-import FollowingPosts from './components/FollowingPosts'
+import "./App.css"
+import { checkSession } from "./services/Auth"
+import Nav from "./components/Nav"
+import Profile from "./components/Profile"
+import SignIn from "./components/SignIn"
+import Register from "./components/Register"
+import Home from "./components/Home"
+import FollowingPosts from "./components/FollowingPosts"
 import Post from "./components/Post"
 import Hashtag from "./components/Hashtag"
-
 
 function App() {
   const [user, setUser] = useState(null)
@@ -44,14 +43,14 @@ function App() {
           <Route path="/" element={<Home user={user} />} />
           <Route path="/signin" element={<SignIn setUser={setUser} />} />
           <Route path="/register" element={<Register setUser={setUser} />} />
-
           <Route path="/profile" element={<Profile user={user} />} />
+          <Route path="/craetePpost" element={<Post user={user} />} />
+
           <Route
             path="/followingPosts"
             element={<FollowingPosts user={user} />}
           />
         </Routes>
-        <Post user={user}/>
       </main>
     </div>
   )
