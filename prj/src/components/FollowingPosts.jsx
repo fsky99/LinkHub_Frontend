3f9511261a58c4896a34625c2198433bc5a12697
fatima <fatimafouad9999@gmail.com>
2024-03-05 00:24:02 +0300

    add comment

--- prj/src/components/FollowingPosts.jsx
+++ prj/src/components/FollowingPosts.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Sidebar from './Sidebar'
-import Client from '../services/api'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import Sidebar from "./Sidebar"
+import Client from "../services/api"
 
 const FollowingPosts = ({ user, users }) => {
   const BASE_URL = import.meta.env.VITE_BASE_URL
   const [listUsers, setListUsers] = useState([])
   const [postList, setPostList] = useState([])
-  const [likes, setLikes] = useState('')
-
+  const [likes, setLikes] = useState("")
+  const [comments, setComments] = useState("")
   const [isLike, setIsLike] = useState(false)
 
   const [loggedInUser, setLoggedInUser] = useState(null)
@@ -17,6 +17,44 @@ const FollowingPosts = ({ user, users }) => {
     getFollowingPosts()
     findLoggedInUser()
   }, [isLike])
+
+  const addComment = async (id, commentText) => {
+    try {
+      const postResponse = await Client.get(`/post/${id}`)
+      const postToUpdate = postResponse.data
+
+      if (!postToUpdate.comments) {
+        postToUpdate.comments = []
+      }
+
+      const newComment = {
+        comment: commentText,
+        date: new Date().toISOString(),
+        userId: user.id,
+      }
+      await Client.post(`/comment`, newComment)
+
+      // postToUpdate.comments.push(newComment)
+
+      // const updateResponse = await Client.put(`/post/${id}`, postToUpdate)
+
+      // if (updateResponse.status === 200) {
+      //   setPostList((prevPostList) => {
+      //     return prevPostList.map((post) => {
+      //       if (post._id === id) {
+      //         return {
+      //           ...post,
+      //           comments: postToUpdate.comments,
+      //         }
+      //       }
+      //       return post
+      //     })
+      //   })
+      // }
+    } catch (error) {
+      console.error("Error adding comment:", error)
+    }
+  }
 
   const handleLikes = async (event, id) => {
     const postToUpdate = await Client.get(`/post/${id}`)
@@ -33,7 +71,7 @@ const FollowingPosts = ({ user, users }) => {
       //setLikes(likes + 1)
       //setPostList(postList)
     } else {
-      console.log('already liked the post')
+      console.log("already liked the post")
     }
   }
 
@@ -83,6 +121,7 @@ const FollowingPosts = ({ user, users }) => {
         {postList
           ? postList.map((p) => (
               <div key={p._id}>
+                <img src={p.image} />
                 <p>{p.text}</p>
 
                 <button
@@ -95,9 +134,29 @@ const FollowingPosts = ({ user, users }) => {
                   LIKE
                 </button>
                 {p.like && <p>likes: {p.like.length} </p>}
+                <h4>comments:</h4>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault()
+                    const commentText = event.target.elements.commentText.value
+                    if (commentText.trim() !== "") {
+                      addComment(p._id, commentText)
+                    }
+                  }}
+                >
+                  <input
+                    type="text"
+                    name="commentText"
+                    placeholder="Add Comment"
+                  />
+                  <button id="btn" type="submit">
+                    Add
+                  </button>
+                </form>
+                {p.comment}
               </div>
             ))
-          : console.log('error')}
+          : console.log("error")}
       </div>
     </div>
   )
