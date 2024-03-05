098ac80acb37bfa7d479933a782220de3f559130
Fatima Fouad <63733736+fsky99@users.noreply.github.com>
2024-03-05 11:21:08 +0300

    Revert "Hamad to main"

--- prj/src/components/Nav.jsx
+++ prj/src/components/Nav.jsx
@@ -2,15 +2,12 @@ import { Link } from "react-router-dom"
 import "../App.css"
 
 const Nav = ({ user, handleLogOut }) => {
-
+  console.log("user:", user)
 
   const userOptions = (
-    <nav id="navbar">
-      {/* <img  src="../img/icon.png" alt="image" /> */}
-
-<img src="../img/icon.png" alt="image" />
-
-      <Link className="linkstochange link-hub-logo" to="/" >
+    <nav>
+      {/* <img src="https://picsum.photos/seed/picsum/200/300" alt="" /> */}
+      <Link className="linkstochange" to="/">
         LinkHub
       </Link>
 
@@ -19,7 +16,7 @@ const Nav = ({ user, handleLogOut }) => {
       </Link>
 
       <Link className="linkstochange" to="/followingPosts">
-        Following posts 
+        Following posts
       </Link>
 
       <Link className="linkstochange" to="/profile">
@@ -31,7 +28,7 @@ const Nav = ({ user, handleLogOut }) => {
 
       <Link className="linkstochange" to="/signin" onClick={handleLogOut}>
         Sign out
-      </Link> 
+      </Link>
       {/* Create Post */}
     </nav>
   )
@@ -39,7 +36,6 @@ const Nav = ({ user, handleLogOut }) => {
   const publicOptions = (
     <nav>
       {/* <img src="https://picsum.photos/seed/picsum/200/300" alt="" /> */}
-      
       <Link to="/">LinkHub</Link>
       <Link to="/">Home</Link>
       <Link to="/register">Register</Link>
