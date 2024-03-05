16d3c5363f964058d55353cbb0a84801de387476
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-03-05 09:00:47 +0300

    create findfollowing function

--- prj/src/components/UsersProfile.jsx
+++ prj/src/components/UsersProfile.jsx
@@ -3,21 +3,25 @@ import { useNavigate, useParams, Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const UsersProfile = (props) => {
+const UsersProfile = ({ user }) => {
   let { id } = useParams()
   const BASE_URL = import.meta.env.VITE_BASE_URL
   const [userProfile, setUserProfile] = useState(null)
+  const [loggedInData, setLoggedInData] = useState(null)
+  const [follow, setFollow] = useState(false)
 
   useEffect(() => {
     finduser()
+    findFollowing()
   }, [])
 
   const finduser = async () => {
-    console.log('props:', props)
+    console.log('props:', user)
     const res = await axios.get(`${BASE_URL}/user/${id}`)
     setUserProfile(res.data)
     console.log('userDatafrom:', res)
   }
+  const findFollowing = async () => {}
   return (
     <div>
       {userProfile ? <p>{userProfile.userName}</p> : <p>Hi</p>}
@@ -33,6 +37,18 @@ const UsersProfile = (props) => {
       ) : (
         <p>followers: 0</p>
       )}
+
+      <div>
+        {userProfile
+          ? userProfile.posts.map((usrp) => (
+              <div key={usrp._id}>
+                <p>{usrp.text}</p>
+                <img src={usrp.image} alt="" />
+                {usrp.like && <p>likes: {usrp.like.length} </p>}
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   )
 }
